9d830383ea8e7d7944ff54d2e0bd2cd3b3046950
tbriggs1 <tombriggs1993@outlook.com>
2021-12-06 20:46:01 +0000

    Added redux boiler code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Homepage from "./components/Homepage/Homepage.js";
 import '../src/styles/style.css';
+import GetBlogs from "./components/Blog/GetBlogs.js";
 
 function App() {
+
   return (
     <div className="App">
-       <Homepage />
+      <GetBlogs />
+      <Homepage />
     </div>
   );
 }
--- src/components/Blog/GetBlogs.js
+++ src/components/Blog/GetBlogs.js
@@ -1,6 +1,23 @@
-import React from 'react';
+// import React, { useEffect } from 'react';
+// import axios from 'axios';
+
+
 
 const GetBlogs = ({blogs}) => {
+    // const options = {
+    //     method: 'GET',
+    //     url: 'https://pokeapi.co/api/v2/pokemon/ditto',
+    //   };
+
+    // useEffect(() => {
+    //     axios.request(options).then(function (response) {
+    //         console.log(response.data);
+    //     }).catch(function (error) {
+    //         console.error(error);
+    //     });   
+    // }, [])
+
+
     return(
         <div>
             <print>{blogs}</print>
