b03e3db84331347af36bf3bcdf7ed7522b9b03a7
tbriggs1 <tombriggs1993@outlook.com>
2021-12-08 21:15:33 +0000

    testing env changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Homepage from "./components/Homepage/Homepage.js";
 import '../src/styles/style.css';
 import Blogs from "./components/Blog/Blogs.js";
 
-
+// Adding comment for testing
 function App() {
 
   return (
