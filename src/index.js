8eec7e1ec44342c366b4104af3d68c254868a85b
tbriggs1 <tombriggs1993@outlook.com>
2022-02-22 14:39:17 +0000

    Fixed starting errors

--- src/index.js
+++ src/index.js
@@ -2,23 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore, applyMiddleware, compose} from 'redux';
-import allReducers from './reducers/index';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-
-const composeEnchaner = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-
-const store = createStore(allReducers, composeEnchaner(applyMiddleware(thunk)));
-
-// window.__REDUX_DEVTOOLS_EXTENSION__ / window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
